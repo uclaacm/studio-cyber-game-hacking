8e809f0f79ee7536fe392ee67c6aac55e9302824
Aaron Isara <68300841+aaisara12@users.noreply.github.com>
2023-09-30 17:14:27 -0700

    Hastily added walls in the game so that player cannot escape camera view

--- Invaders/Assets/Scripts/UnitMovement.cs
+++ Invaders/Assets/Scripts/UnitMovement.cs
@@ -9,6 +9,9 @@ public class UnitMovement : MonoBehaviour
     [SerializeField] float _speed = 2;
     [SerializeField] float smoothTime = 0.2f;
 
+    [SerializeField] Transform lowerLeftBounds;
+    [SerializeField] Transform upperRightBounds;
+
     Vector2 currentVelocity = Vector2.zero;
     Vector2 targetVelocity = Vector2.zero;
     Vector2 currentVel;
@@ -31,5 +34,25 @@ public class UnitMovement : MonoBehaviour
         targetVelocity = input.inputVector.normalized;
         currentVelocity = Vector2.SmoothDamp(currentVelocity, targetVelocity, ref currentVel, smoothTime);
         transform.Translate((currentVelocity) * _speed * Time.deltaTime);
+
+
+        // Ensure unit stays within bounds
+        // NOTE: This is a very hasty implementation for implementing walls in the game. In a real game we'd
+        // probably like to keep this in a separate component away from the movement provider. Perhaps in the
+        // main player control module or something like that.
+        if (lowerLeftBounds == null || upperRightBounds == null)
+            return;
+
+        Vector3 finalPos = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        if(transform.position.x > upperRightBounds.position.x)
+            finalPos.x = upperRightBounds.position.x;
+        if(transform.position.x < lowerLeftBounds.position.x)
+            finalPos.x = lowerLeftBounds.position.x;
+        if(transform.position.y > upperRightBounds.position.y)
+            finalPos.y = upperRightBounds.position.y;
+        if(transform.position.y < lowerLeftBounds.position.y)
+            finalPos.y = lowerLeftBounds.position.y;
+
+        transform.position = finalPos;
     }
 }
